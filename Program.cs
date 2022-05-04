4aa32645ab66d17726583325d30a9c7ce9073ad0
ct88ice <ct88ice@gmail.com>
2022-05-04 13:26:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using GameMenu2;
+
+while (true)
+{
+    StartMenu();
+}
+
+
+void StartMenu()
+{
+    Console.WriteLine("Pick a game:\n1. Tiktaktoe\n2. Snake\n3. Exit");
+    switch (Console.ReadKey(true).Key)
+    {
+        case ConsoleKey.D1:
+            new Tiktaktoe().Start();
+            break;
+        case ConsoleKey.D2:
+            new Snake().Start();
+            break;
+        case ConsoleKey.D3:
+            Environment.Exit(0);
+            break;
+        default:
+            break;
+    }
+}
